f39ad114d22bf4d991c5ad98ccd7df08aff09090
kanhua <kanhwa@gmail.com>
2017-03-17 11:58:55 +0900

    add more data

--- nvidia_model.py
+++ nvidia_model.py
@@ -23,8 +23,10 @@ img_sub_foler = 'IMG/'
 ch, row, col = 3, 160, 320
 ch, p_row, p_col = 3, 160, 320
 train_dataset_folder = ["official_baseline/","trip1_off_recover/",
-                        "track2_1/","track2_2/","track2_3/","track2_rec_1"]
-train_side_camera=False
+                        "track2_1/","track2_2/","track2_3/",
+                        "track2_4/","track2_5/",
+                        "track2_rec_1","track2_rec_2"]
+train_side_camera=True
 batch_size = 128
 
 
@@ -200,8 +202,8 @@ def main(_):
     #with open('model_hist.p','wb') as fp:
     #    pickle.dump(hist['loss'],fp)
 
-    nvidia_model.save("model_v9.h5")
-    nvidia_model.save_weights('nvidia_model_weights_v9.h5')
+    nvidia_model.save("model_v10.h5")
+    nvidia_model.save_weights('nvidia_model_weights_v10.h5')
 
 
 # parses flags and calls the `main` function above
